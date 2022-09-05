2e7db9b842b1539f8c4b05f6d0f93d2872adfd3f
Alex Reynish <reynish@gmail.com>
2022-09-05 15:24:05 +0100

    Floor

--- src/index.js
+++ src/index.js
@@ -27,12 +27,38 @@ var createDefaultEngine = function () {
 
 var createScene = async function () {
   await xrPolyfillPromise;
-  console.log(navigator.xr); // should be there!
   console.log(
     await BABYLON.WebXRSessionManager.IsSessionSupportedAsync("immersive-vr")
-  ); // should be true
-  // This creates a basic Babylon Scene object (non-mesh)
+  );
   var scene = new BABYLON.Scene(engine);
+
+  var environment = scene.createDefaultEnvironment({
+    createGround: false,
+    skyboxSize: 1000
+  });
+  environment.setMainColor(BABYLON.Color3.FromHexString("#74b9ff"));
+  var ground = BABYLON.MeshBuilder.CreateGround(
+    "ground",
+    { width: 1000, height: 1000 },
+    scene
+  );
+  ground.physicsImpostor = new BABYLON.PhysicsImpostor(
+    ground,
+    BABYLON.PhysicsImpostor.BoxImpostor,
+    {
+      mass: 0,
+      friction: 0.8,
+      restitution: 0.5,
+      disableBidirectionalTransformation: true
+    },
+    scene
+  );
+  ground.checkCollisions = true;
+  // ground.material = new BABYLON.GridMaterial("mat", scene);
+  ground.material = await BABYLON.NodeMaterial.ParseFromSnippetAsync(
+    "#I4DJ9Z#4",
+    scene
+  );
 
   // This creates and positions a free camera (non-mesh)
   var camera = new BABYLON.FreeCamera(
@@ -48,48 +74,30 @@ var createScene = async function () {
   camera.attachControl(canvas, true);
 
   // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-  var light = new BABYLON.PointLight(
-    "pointLight",
-    new BABYLON.Vector3(10, 10, 0),
+  var light = new BABYLON.HemisphericLight(
+    "light",
+    new BABYLON.Vector3(0, 1, 0),
     scene
   );
 
   // Default intensity is 1. Let's dim the light a small amount
   light.intensity = 0.7;
 
-  var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+  // Our built-in 'sphere' shape.
+  var sphere = BABYLON.MeshBuilder.CreateSphere(
+    "sphere",
+    { diameter: 2, segments: 32 },
+    scene
+  );
+
+  // Move the sphere upward 1/2 its height
   sphere.position.y = 1;
 
-  var plane = BABYLON.Mesh.CreatePlane("plane", 2);
-  plane.parent = sphere;
-  plane.position.y = 2;
-
-  var advancedTexture = GUI.AdvancedDynamicTexture.CreateForMesh(plane);
-  scene.clearColor = new BABYLON.Color3(0.5, 0.8, 0.5);
-  var button1 = GUI.Button.CreateSimpleButton("but1", "Click Me");
-  button1.width = 1;
-  button1.height = 0.4;
-  button1.color = "white";
-  button1.fontSize = 50;
-  button1.background = "green";
-  button1.onPointerUpObservable.add(function () {
-    alert(scene.ambientColor);
+  // XR
+  const xrHelper = await scene.createDefaultXRExperienceAsync({
+    floorMeshes: [environment.ground]
   });
-  advancedTexture.addControl(button1);
-
-  // Our built-in 'ground' shape.
-  // var ground = BABYLON.MeshBuilder.CreateGround(
-  //   "ground",
-  //   { width: 6, height: 6 },
-  //   scene
-  // );
-
-  const env = scene.createDefaultEnvironment();
-
-  const xr = await scene.createDefaultXRExperienceAsync({
-    floorMeshes: [env.ground]
-  });
-
+  scene.debugLayer.show();
   return scene;
 };
 
